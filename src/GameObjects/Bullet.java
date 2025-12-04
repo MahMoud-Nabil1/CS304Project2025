68ac80e3764b1079a6d74e43697efa5d983be237
MahMoud-Nabil1 <mahmoudiiwork@gmail.com>
2025-12-04 09:28:58 +0200

    edits

--- src/GameObjects/Bullet.java
+++
@@ -1,9 +0,0 @@
-package GameObjects;
-
-public class Bullet extends GameObject {
-    public Bullet(int posX,int posY,int speed, int damage) {
-        super(posX,posY);
-        this.posX = posX;
-        this.posY = posY;
-    }
-}
